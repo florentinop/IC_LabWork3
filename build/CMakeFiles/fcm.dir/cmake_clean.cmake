614207521efacf9a76a78fac9d61f814233f912e
rt <florentino@ua.pt>
2022-12-05 16:04:59 +0000

    start

---
+++ build/CMakeFiles/fcm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/rt/Desktop/IC_LabWork3/bin/fcm"
+  "/home/rt/Desktop/IC_LabWork3/bin/fcm.pdb"
+  "CMakeFiles/fcm.dir/fcm.cpp.o"
+  "CMakeFiles/fcm.dir/fcm.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fcm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
